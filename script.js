86bed5fc41212bb50898f05ef2cd076a57db14ee
pwj-v <melissa.velasco093@gmail.com>
2021-03-17 18:34:51 -0400

    Added event listeners to the buttons that  will run the game code each time it is pressed. game code contains 3 functions. One that runs the actual rock, paper, scissors game against the computer and the other two focus on the scores. One of the two functions adds points and outputs text onto the webpage, and the second one checks if a player has reached five points. If and when a player hits five points, an alert displaying the winner will run.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
 function computerplay() {
     function numPick(){ 
         let numberGenerator = Math.floor(Math.random() * 3) + 1; 
@@ -36,4 +40,62 @@ function playRound(playerSelection, computerSelection) {
              return 'Something went wrong. Make sure you enter a valid input.';
              break;
         }
+}
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    game();
+}); 
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    game();
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    playerSelection = 'paper';
+    game();
+});
+
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+const tie = document.querySelector('#tie');
+
+function scores(text) {
+        if (text == 'You won!' && playerScore != 5) {
+            playerScore += 1;
+            player.textContent = `Player: ${playerScore}`;
+        }
+        else if (text == 'You lost.' && computerScore != 5) {
+            computerScore += 1;
+            computer.textContent = `Computer: ${computerScore}`;
+        }
+        else if (text == 'Tie.') {
+            tieScore += 1;
+            tie.textContent = `Tie: ${tieScore}`;
+        }
+    // console.log(playerScore, computerScore, tieScore);
+}
+
+function check() {
+    if (playerScore == 5) {
+        alert('You beat the AI!');
     }
+    else if (computerScore == 5) {
+        alert('You got beat by an AI.');
+    }
+}
+
+function game() {
+    const computerSelection = computerplay();
+    playRound(playerSelection, computerSelection);
+    const div = document.createElement('div');
+    div.textContent = playRound(playerSelection, computerSelection);
+    resultsContainer.appendChild(div);
+    text = div.textContent;
+    scores(text);
+    check();
+}
